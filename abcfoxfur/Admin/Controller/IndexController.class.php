c3535e554be25a8ab92d9baa2a700ff615406028
Michie <oriesai@gmail.com>
2017-06-13 13:56:40 +0800

    added view of products, controllers and built eco_products db

---
+++ abcfoxfur/Admin/Controller/IndexController.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function left()
+    {
+        $this->display();
+    }
+
+    public function top()
+    {
+        $this->display();
+    }
+
+    public function main()
+    {
+        $this->display();
+    }
+}
